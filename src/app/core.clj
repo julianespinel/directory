945d32ec71fed911d4437cd924014b0f1a84652d
Julian Espinel <julianespinel@gmail.com>
2013-11-03 18:45:52 -0500

    Refactor the 'GET /user/:id' route. Create a new function in order to make the code more readable.

--- src/app/core.clj
+++ src/app/core.clj
@@ -3,11 +3,12 @@
             [compojure.core :refer [defroutes GET]]))
 
 (defn ok-status [] "200")
+(defn get-user-message [id] (str "Hello user: " id))
 (defn not-found-error [] "404")
 
 (defroutes app
   (GET "/" [] (ok-status))
-  (GET "/user/:id" [id] (str "Hello user: " id))
+  (GET "/user/:id" [id] (get-user-message id))
   (GET "/unknown" [] (not-found-error)))
 
 (run-jetty #'app {:port 3000})
