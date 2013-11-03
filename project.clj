371a0f940b6ed48ae03261b94c3b2bd985619a18
Julian Espinel <julianespinel@gmail.com>
2013-11-03 01:32:07 -0500

    Add first REST service at GET localhost:3000/

--- project.clj
+++ project.clj
@@ -1,3 +1,7 @@
 (defproject app "1.0.0-SNAPSHOT"
   :description "FIXME: write description"
-  :dependencies [[org.clojure/clojure "1.3.0"]])
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [liberator "0.9.0"]
+                 [compojure "1.1.3"]
+                 [ring/ring-jetty-adapter "1.1.0"]]
+  :main app.core)
--- src/app/core.clj
+++ src/app/core.clj
@@ -1,1 +1,11 @@
-(ns app.core)
+(ns app.core
+  (:require [liberator.core :refer [resource defresource]]
+            [ring.adapter.jetty :refer [run-jetty]]
+            [compojure.core :refer [defroutes ANY]]))
+
+(defn ok-status [] "200")
+
+(defroutes app
+  (ANY "/" [] (ok-status)))
+
+(run-jetty #'app {:port 3000})
