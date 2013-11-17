6d5df1f306be437e73140ceabd467daa76ecdbae
Julian Espinel <julianespinel@gmail.com>
2013-11-16 23:03:29 -0500

    Add some tests for compojure routes.

--- test/directory/test/handler.clj
+++ test/directory/test/handler.clj
@@ -4,10 +4,19 @@
         directory.handler))
 
 (deftest test-app
-  (testing "main route"
+  (testing "Index"
     (let [response (app (request :get "/"))]
+      (is (= (:status response) 302)) ; Redirect to /redirect-url
+      (is (= (:body response) ""))))
+
+  (testing "Redirect url"
+    (let [response (app (request :get "/redirect-url"))]
       (is (= (:status response) 200))
-      (is (= (:body response) "Hello World"))))
+      (is (= (:body response) "Hello you have been redirected."))))
+
+  (testing "Register service no payload"
+    (let [response (app (request :post "/services"))]
+      (is (= (:status response) 400)))) ; The body payload is missing.
   
   (testing "not-found route"
     (let [response (app (request :get "/invalid"))]
