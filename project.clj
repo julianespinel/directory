f8f252b98afb0f162ad9cbfce6a69dc153d2d203
Julian Espinel <julianespinel@gmail.com>
2014-01-28 00:41:57 -0500

    Update monger dependency to version 1.7.0.

--- project.clj
+++ project.clj
@@ -4,7 +4,7 @@
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [ring/ring-json "0.2.0"]
                  [compojure "1.1.6"]
-                 [com.novemberain/monger "1.6.0"]]
+                 [com.novemberain/monger "1.7.0"]]
   :plugins [[lein-ring "0.8.8"]]
   :ring {:handler directory.handler/app
          :stacktraces? true
