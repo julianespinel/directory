8a16699147f4342644f1d557e015ed5118390ee5
Julian Espinel <julianespinel@gmail.com>
2014-09-14 16:58:46 -0500

    Allow the Directory system to be an executable jar.

--- project.clj
+++ project.clj
@@ -3,6 +3,7 @@
   :url "http://example.com/FIXME"
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [ring/ring-json "0.2.0"]
+                 [ring/ring-jetty-adapter "1.2.2"]
                  [compojure "1.1.6"]
                  [com.novemberain/monger "1.7.0"]]
   :plugins [[lein-ring "0.8.8"]]
@@ -15,4 +16,5 @@
          :dependencies [[javax.servlet/servlet-api "2.5"]
                         [ring-mock "0.1.5"]
                         [midje "1.6.2"]
-                        [cheshire "5.2.0"]]}})
+                        [cheshire "5.2.0"]]}}
+  :main directory.main)
--- src/directory/main.clj
+++ src/directory/main.clj
@@ -1,10 +1,18 @@
 (ns directory.main
   "The main file of the application."
+  (:gen-class)
   (:use ring.middleware.json)
-  (:require [directory.services.microserviceApi :as microserviceApi]
-            [compojure.handler :as handler]))
+  (:require [ring.adapter.jetty :as jetty]
+            [compojure.handler :as handler]
+            [directory.services.microserviceApi :as microserviceApi]))
 
 (def app
   (-> (handler/api microserviceApi/api-routes)
       (wrap-json-body)
       (wrap-json-response)))
+
+(defn start-server []
+  (jetty/run-jetty app {:port 4001 :join? false}))
+
+(defn -main [& args] 
+  (start-server))
