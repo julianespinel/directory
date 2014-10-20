9542436bb9dc08413b6bce947fc5a40576676ea7
Julian Espinel <julianespinel@gmail.com>
2014-10-19 20:43:20 -0500

    Add logging middleware.

--- src/directory/main.clj
+++ src/directory/main.clj
@@ -4,12 +4,14 @@
   (:use [ring.middleware.json])
   (:require [org.httpkit.server :as http-kit]
             [compojure.handler :as handler]
+            [directory.infrastructure.middleware.logging-middleware :as logging]
             [directory.services.microserviceApi :as microserviceApi]))
 
 (def app
   (-> (handler/api microserviceApi/api-routes)
       (wrap-json-body)
-      (wrap-json-response)))
+      (wrap-json-response)
+      (logging/simple-logggin-middleware)))
 
 (defn start-server []
   (http-kit/run-server app {:port 4001}))
